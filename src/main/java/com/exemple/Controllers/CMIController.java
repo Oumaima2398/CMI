66de21d2b40629c92ccd1d4050a81f65a8a63609
Oumaima2398 <65559150+Oumaima2398@users.noreply.github.com>
2021-01-30 20:39:08 +0100

    msg

--- src/main/java/com/exemple/Controllers/CMIController.java
+++ src/main/java/com/exemple/Controllers/CMIController.java
@@ -1,4 +1,12 @@
 package com.exemple.Controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CMIController {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello";
+    }
 }
--- src/main/java/com/exemple/Services/CMIService.java
+++ src/main/java/com/exemple/Services/CMIService.java
@@ -1,4 +1,41 @@
 package com.exemple.Services;
 
+import com.exemple.Entities.Facture;
+import com.exemple.Entities.Payement;
+import com.exemple.Entities.User;
+import com.exemple.Rep.CMIRepository;
+
 public class CMIService {
+    public boolean paye;
+    private Boolean permission;
+    private User user;
+    private boolean i;
+
+    public void verifySolde(double solde) {
+        if(CMIRepository.getsolde() > solde ){
+            i = true;
+            Payement p = new Payement();
+        }
+        else{
+            i = false;
+        }
+    }
+
+    public void verifyBlackList() {
+        if(CMIRepository.BlackList().contains(user.telephone) ){
+            permission = false;
+        }
+        else{
+            permission = true;
+        }
+    }
+    public void verifyPayee(Facture idf) {
+
+        if(CMIRepository.DejaPayes().contains(idf) ){
+            paye = true;
+        }
+        else{
+            paye = false;
+        }
+    }
 }
